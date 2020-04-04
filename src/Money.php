5f9ea3c3767b4fcf0717d6f6648ed5fc279ef1b1
Y.Maeshima <ymzlgcranch@gmail.com>
2020-04-04 21:29:37 +0900

    8章まで

--- src/Money.php
+++ src/Money.php
@@ -4,13 +4,25 @@
 namespace app;
 
 
-class Money
+abstract class Money
 {
     protected $amount;
+
+    abstract public function times(int $multiplier);
 
     public function equals(Money $money): bool
     {
         return $this->amount == $money->amount
             && get_class($this) == get_class($money);
+    }
+
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount);
     }
 }
