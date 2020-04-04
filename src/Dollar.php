80e4c4471883a589b4129b448b8a0be24b6d39c2
Y.Maeshima <ymzlgcranch@gmail.com>
2020-04-04 17:38:32 +0900

    ３章終わりまで

--- src/Dollar.php
+++ src/Dollar.php
@@ -5,13 +5,19 @@ namespace app;
 class Dollar
 {
     public $amount;
+
     public function __construct(int $amount)
     {
         $this->amount = $amount;
     }
 
-    public function times(int $multiplier)
+    public function times(int $multiplier): Dollar
     {
         return new Dollar($this->amount * $multiplier);
+    }
+
+    public function equals(Dollar $dollar): bool
+    {
+        return $this->amount === $dollar->amount;
     }
 }
