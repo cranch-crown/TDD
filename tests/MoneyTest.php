ff724feec24f2a6a3c08ef5b0a8eccfa61b44661
Y.Maeshima <ymzlgcranch@gmail.com>
2020-04-05 12:23:10 +0900

    11章終わりまで

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -19,13 +19,6 @@ class MoneyTest extends TestCase
         $this->assertFalse(Money::franc(5)->equals(Money::dollar(5)));
     }
     
-    public function testFrancMultiplication(): void
-    {
-        $five = Money::franc(5);
-        $this->assertEquals(Money::franc(10), $five->times(2));
-        $this->assertEquals(Money::franc(15), $five->times(3));
-    }
-
     public function testCurrency(): void
     {
         $this->assertEquals('USD', Money::dollar(1)->currency());
