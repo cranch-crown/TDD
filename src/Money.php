e7d81757d9983114fd0e3d6b549d4d1c222cf97e
Y.Maeshima <ymzlgcranch@gmail.com>
2020-04-05 11:49:46 +0900

    10章終わり

--- src/Money.php
+++ src/Money.php
@@ -4,7 +4,7 @@
 namespace app;
 
 
-abstract class Money
+class Money
 {
     protected $amount;
 
@@ -16,7 +16,10 @@ abstract class Money
         $this->currency = $currency;
     }
 
-    abstract public function times(int $multiplier): Money;
+    public function times(int $multiplier): Money
+    {
+        return new Money($this->amount * $multiplier, $this->currency);
+    }
 
     public function currency(): string
     {
@@ -26,16 +29,16 @@ abstract class Money
     public function equals(Money $money): bool
     {
         return $this->amount == $money->amount
-            && get_class($this) == get_class($money);
+            && $this->currency() == $money->currency();
     }
 
     public static function dollar(int $amount): Money
     {
-        return new Dollar($amount, 'USD');
+        return new Money($amount, 'USD');
     }
 
     public static function franc(int $amount): Money
     {
-        return new Franc($amount, 'CHF');
+        return new Money($amount, 'CHF');
     }
 }
